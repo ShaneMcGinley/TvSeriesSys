f29e17335eddc61daf5e8cc350b8f159e3e320ce
Shane McGinley <shane.mcginley@students.ittralee.ie>
2020-10-18 17:26:28 +0100

    Moved Create, update code to their own buttons

--- TvSeries/MainWindow.xaml.cs
+++ TvSeries/MainWindow.xaml.cs
@@ -9,43 +9,39 @@ namespace TvSeries
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
-        
-
-            /*using (var client = new MyCouchClient("http://admin:admin@localhost:5984", "tv-series"))
-            {
-                //Consume here
-            }*/
-        
-
-        
+    {  
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var client = new MyCouchClient("http://admin:admin@localhost:5984", "tv-series"))
+            {
+                //POST with server generated id
+                await client.Documents.PostAsync("{\"name\":\"Daniel\"}");
+            }
+        }
+
+        private async void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var client = new MyCouchClient("http://admin:admin@localhost:5984", "tv-series"))
+            {
+                //PUT for updates
+                await client.Documents.PutAsync("e4862b8c65a4677d4f4fa3d4cf002228", "1-6824e4a536fdf83a246/eca26b943dfe0", "{\"name\":\"Daniel Wertheim\"}");
+            }
         }
 
         private async void DeleteButton_Click(object sender, RoutedEventArgs e)
         {
             using (var client = new MyCouchClient("http://admin:admin@localhost:5984", "tv-series"))
             {
-
-                //PUT THESE IN THERE OWN BUTTONS
-
-
-                //POST with server generated id
-                await client.Documents.PostAsync("{\"name\":\"Daniel\"}");
-
-                //PUT for updates
-                await client.Documents.PutAsync("e4862b8c65a4677d4f4fa3d4cf002228", "1-6824e4a536fdf83a246eca26b943dfe0", "{\"name\":\"Daniel Wertheim\"}");
-
                 //Delete a document
                 await client.Documents.DeleteAsync("e4862b8c65a4677d4f4fa3d4cf002228", "2-9b42031d85a6c397b4f2d51a4b8698ec");
 
+                //Read a document - Not yet working
                 example.Text = Convert.ToString(await client.Documents.GetAsync("e4862b8c65a4677d4f4fa3d4cf00755e"));
-
-
-
-
 
                 //PUT for client generated id
                 //await client.Documents.PutAsync("e4862b8c65a4677d4f4fa3d4cf002228", "{\"name\":\"Donald Trump\"}");
@@ -60,18 +56,6 @@ namespace TvSeries
                 //Using anonymous entities
                 await client.Entities.PostAsync(new { Name = "Daniel" });*/
             }
-
-        }
-
-        private void CreateButton_Click(object sender, RoutedEventArgs e)
-        {
-
-
-
-        }
-
-        private void UpdateButton_Click(object sender, RoutedEventArgs e)
-        {
 
         }
     }
