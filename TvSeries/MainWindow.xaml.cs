a288c1da0b4d4b58f9989ad267a140b48f4eb0ba
Shane McGinley <shane.mcginley@students.ittralee.ie>
2020-10-24 00:08:03 +0100

    Updated Comments,  added  MessageBoxes, and a method to center the program

--- TvSeries/MainWindow.xaml.cs
+++ TvSeries/MainWindow.xaml.cs
@@ -14,10 +14,27 @@ namespace TvSeries
         {
             InitializeComponent();
             Loaded += MainWindow_Loaded;
+
+            CenterProgramOnLaunch();
+        }
+
+        // Keeps the Program in the center of the screen when launched.
+        private void CenterProgramOnLaunch()
+        {
+            double screenWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+            double screenHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+            double windowWidth = this.Width;
+            double windowHeight = this.Height;
+            this.Left = (screenWidth / 2) - (windowWidth / 2);
+            this.Top = (screenHeight / 2) - (windowHeight / 2);
         }
 
         private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
+            /* Query / map reduce for counting the number of tv series documents.
+              This is so that the number of tv-serie documents is shown when the program is opened   
+            */
+            //Connecting to the database / fauxton using MyCouch
             using (var client = new MyCouchClient("http://admin:admin@localhost:5984", "tv-series"))
             {
                 var personQuery = new QueryViewRequest("series", "CountNoTvSeries").Configure(query2 => query2
@@ -32,11 +49,12 @@ namespace TvSeries
         {
             using (var store = new MyCouchStore("http://admin:admin@localhost:5984", "tv-series"))
             {
-                //Get hardcoded document ID.
+                //Get User given document ID with MyCouch.
                 var retrieved = await store.GetByIdAsync(SearchIDTxt.Text);
 
                 ReadTxt.Text = retrieved;
 
+                //Aligning the text to be in JSON structure.
                 ReadTxt.Text = ReadTxt.Text.Replace(",", "," + System.Environment.NewLine + "    ");
                 ReadTxt.Text = ReadTxt.Text.Replace("{", "{" + System.Environment.NewLine + "    ");
                 ReadTxt.Text = ReadTxt.Text.Replace("}", System.Environment.NewLine + "}");
@@ -52,17 +70,21 @@ namespace TvSeries
         {
             using (var client = new MyCouchClient("http://admin:admin@localhost:5984", "tv-series"))
             {
-                //POST with server generated id & Rev
+                //POST (create) with server generated id & Rev
                 await client.Documents.PostAsync("{\"title\": \"" + TitleText.Text + "\", \"creator\":\"" + CreatorTxt.Text + "\",\"stars\":\"" + StarsTxt.Text + 
                     "\",\"seasons\":\"" + SeasonsTxt.Text + "\",\"mpa_rating\":\"" + MPARatingTxt.Text + "\",\"imbd_rating\":\"" + IMBDRatingTxt.Text + "/10\"}");
 
-                TitleText.Text = "";
-                CreatorTxt.Text = "";
-                StarsTxt.Text = "";
-                SeasonsTxt.Text = "";
-                MPARatingTxt.Text = "";
-                IMBDRatingTxt.Text = "";
+                MessageBox.Show("Document Successfully Created", "Tv-Series");
 
+                // Clears all text boxes when a document is created.
+                TitleText.Clear();
+                CreatorTxt.Clear();
+                StarsTxt.Clear();
+                SeasonsTxt.Clear();
+                MPARatingTxt.Clear();
+                IMBDRatingTxt.Clear();
+
+                // Refreshes the number of tv-serie documents when one is created.
                 var personQuery = new QueryViewRequest("series", "CountNoTvSeries").Configure(query2 => query2
                 .Reduce(false));
                 ViewQueryResponse result2 = await client.Views.QueryAsync(personQuery);
@@ -75,9 +97,11 @@ namespace TvSeries
         {
             using (var client = new MyCouchClient("http://admin:admin@localhost:5984", "tv-series"))
             {
-                //PUT for updates
+                //PUT for updates with MyCouch
 
                 await client.Documents.PutAsync(SearchIDTxt.Text, ReadTxt.Text);
+
+                MessageBox.Show("Document Successfully Updated", "Tv-Series");
 
                 ReadTxt.Clear();
 
@@ -89,15 +113,18 @@ namespace TvSeries
         {
             using (var store = new MyCouchStore("http://admin:admin@localhost:5984", "tv-series"))
             {
-                //Delete a document
+                //Delete a document using user given ID
 
                 await store.DeleteAsync(SearchIDTxt.Text);
+
+                MessageBox.Show("Document Successfully Deleted", "Tv-Series");
 
                 SearchIDTxt.Text = "";
                 ReadTxt.Text = "";
                 SearchIDTxt.IsEnabled = true;
             }
 
+            // Refreshes the number of tv-serie documents when one is removed.
             using (var client = new MyCouchClient("http://admin:admin@localhost:5984", "tv-series"))
             {
                 var personQuery = new QueryViewRequest("series", "CountNoTvSeries").Configure(query2 => query2
@@ -111,6 +138,8 @@ namespace TvSeries
 
         private void ClearButton_Click(object sender, RoutedEventArgs e)
         {
+            // Clears all boxes in case of mistake etc..
+
             SearchIDTxt.Text = "";
             SearchIDTxt.IsEnabled = true;
             ReadTxt.Text = "";
@@ -120,6 +149,8 @@ namespace TvSeries
             SeasonsTxt.Text = "";
             MPARatingTxt.Text = "";
             IMBDRatingTxt.Text = "";
+
+            MessageBox.Show("All Boxes Have Been Reset", "Tv-Series");
         }
     }
 }
